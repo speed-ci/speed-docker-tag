6770b537372d1653fc296bbf1b4f0559816d69e5
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-27 23:50:45 +0000

    fix(metadata): extract project metadata from git

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -48,9 +48,11 @@ init_env
 REPO_URL=$(git config --get remote.origin.url | sed 's/\.git//g' | sed 's/\/\/.*:.*@/\/\//g')
 GITLAB_URL=`echo $REPO_URL | grep -o 'https\?://[^/]\+/'`
 GITLAB_API_URL="$GITLAB_URL/api/v4"
+PROJECT_NAME=${REPO_URL##*/}
+PROJECT_NAMESPACE=${PROJECT_NAME%-*}
 LAST_COMMIT_ID=$(git log --format="%H" -n 1)
 
-PROJECT_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$CI_PROJECT_NAME" | jq .[0].id`
+PROJECT_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_NAME" | jq .[0].id`
 echo $GITLAB_TOKEN
 echo $GITLAB_API_URL
 echo $PROJECT_ID
@@ -60,7 +62,7 @@ echo "LAST_COMMIT_ID         : $LAST_COMMIT_ID"
 echo "VERSION_ON_LAST_COMMIT : $VERSION_ON_LAST_COMMIT"
 
 if [[ -z $VERSION_ON_LAST_COMMIT ]]; then
-    IMAGE=$ARTIFACTORY_DOCKER_REGISTRY/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME
+    IMAGE=$ARTIFACTORY_DOCKER_REGISTRY/$PROJECT_NAMESPACE/$PROJECT_NAME
     docker pull $IMAGE
     echo "TAGGED_IMAGE : $IMAGE $IMAGE:$VERSION_ON_LAST_COMMIT"
     docker tag $IMAGE $IMAGE:$VERSION_ON_LAST_COMMIT
