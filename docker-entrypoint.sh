569acb560f62f2ff640c338ffc77297d67f2d455
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-27 23:44:47 +0000

    fix(conf): check configuration an source variable file)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,10 @@ printmainstep() {
    # 35 is purple
    echo -e "\033[35m\n== ${1} \033[37m \n"
 }
+printinfo () {
+    # 32 is green
+    echo -e "\033[32m==== INFO : ${1} \033[37m"
+}
 printwarn () {
     # 33 is yellow
     echo -e "\033[33m==== ATTENTION : ${1} \033[37m"
@@ -17,12 +21,30 @@ printerror () {
     # 31 is red
     echo -e "\033[31m==== ERREUR : ${1} \033[37m"
 }
-printinfo () {
-    # 32 is green
-    echo -e "\033[32m==== INFO : ${1} \033[37m"
+
+init_env () {
+    CONF_DIR=/conf/
+    if [ ! -d $CONF_DIR ]; then
+        printerror "Impossible de trouver le dossier de configuration $CONF_DIR sur le runner"
+        exit 1
+    else
+        source $CONF_DIR/variables
+    fi
+    APP_DIR=/usr/src/app/
+    if [ ! -d $APP_DIR ]; then
+        printerror "Impossible de trouver le dossier du code source de l'application $APP_DIR sur le runner"
+        exit 1
+    fi    
+    if [[ -z $GITLAB_TOKEN ]]; then
+        printerror "La variable GITLAB_TOKEN n'est pas présente, sortie..."
+        exit 1
+    fi
 }
 
 printstep "Ajout du tag sur l'image docker latest courante d'Artifactory"
+printstep "Vérification des paramètres d'entrée"
+init_env
+
 REPO_URL=$(git config --get remote.origin.url | sed 's/\.git//g' | sed 's/\/\/.*:.*@/\/\//g')
 GITLAB_URL=`echo $REPO_URL | grep -o 'https\?://[^/]\+/'`
 GITLAB_API_URL="$GITLAB_URL/api/v4"
