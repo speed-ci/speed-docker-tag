3459a91760772498111a2df7f8fbaac791e58fb1
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-28 00:07:07 +0000

    chore(print): display error message if no tag found

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -53,19 +53,14 @@ PROJECT_NAMESPACE=${PROJECT_NAME%-*}
 LAST_COMMIT_ID=$(git log --format="%H" -n 1)
 
 PROJECT_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_NAME" | jq .[0].id`
-echo $GITLAB_TOKEN
-echo $GITLAB_API_URL
-echo $PROJECT_ID
 VERSION_ON_LAST_COMMIT=$(curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/repository/tags" | jq --arg commit_id "$LAST_COMMIT_ID" '.[] | select(.commit.id == "\($commit_id)")' | jq .name | tr -d '"')
-
-echo "LAST_COMMIT_ID         : $LAST_COMMIT_ID"
-echo "VERSION_ON_LAST_COMMIT : $VERSION_ON_LAST_COMMIT"
 
 if [[ -n $VERSION_ON_LAST_COMMIT ]]; then
     IMAGE=$ARTIFACTORY_DOCKER_REGISTRY/$PROJECT_NAMESPACE/$PROJECT_NAME
     docker pull $IMAGE
-    echo "TAGGED_IMAGE : $IMAGE $IMAGE:$VERSION_ON_LAST_COMMIT"
     docker tag $IMAGE $IMAGE:$VERSION_ON_LAST_COMMIT
     docker login -u $ARTIFACTORY_CI_USER -p $ARTIFACTORY_CI_PASSWORD $ARTIFACTORY_DOCKER_REGISTRY
     docker push $IMAGE:$VERSION_ON_LAST_COMMIT
+else
+    printerror "Aucun tag trouvé sur le dernier commit $LAST_COMMIT_ID"
 fi
