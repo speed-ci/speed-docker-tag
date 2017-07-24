68a38bd7305c8a9c50217c5958f29802ebdce488
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-07-24 16:18:52 +1100

    refactor(curl): use myCurl to reveal hidden potential errors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,11 +11,11 @@ int_gitlab_api_env
 LAST_COMMIT_ID=$(git log --format="%H" -n 1)
 
 IMAGE=$ARTIFACTORY_DOCKER_REGISTRY/$PROJECT_NAMESPACE/$PROJECT_NAME
-PROJECT_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_NAME" | jq .[0].id`
-VERSION_ON_LAST_COMMIT=$(curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/repository/tags" | jq --arg commit_id "$LAST_COMMIT_ID" '.[] | select(.commit.id == "\($commit_id)")' | jq .name | tr -d '"')
+PROJECT_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_NAME" | jq .[0].id`
+VERSION_ON_LAST_COMMIT=$(myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_ID/repository/tags" | jq -r --arg commit_id "$LAST_COMMIT_ID" '.[] | select(.commit.id == "\($commit_id)") | .name')
 
 if [[ -n $VERSION_ON_LAST_COMMIT ]]; then
-    ARTIFACTORY_IMAGE_ID=`curl -u $ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD --silent --noproxy '*' "$ARTIFACTORY_URL/artifactory/docker/$PROJECT_NAMESPACE/$PROJECT_NAME/$VERSION_ON_LAST_COMMIT/manifest.json" | jq .config.digest | tr -d '"'`
+    ARTIFACTORY_IMAGE_ID=`myCurl -u $ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD "$ARTIFACTORY_URL/artifactory/docker/$PROJECT_NAMESPACE/$PROJECT_NAME/$VERSION_ON_LAST_COMMIT/manifest.json" | jq -r .config.digest`
     if [[ $ARTIFACTORY_IMAGE_ID == "null" ]]; then
         docker login -u $ARTIFACTORY_USER -p $ARTIFACTORY_PASSWORD $ARTIFACTORY_DOCKER_REGISTRY
         docker pull $IMAGE
